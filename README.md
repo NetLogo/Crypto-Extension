92ae5c492afabf7d112e724a1c544efdbc4ad914
TheBizzle <jason.bertsche@gmail.com>
2013-04-20 01:56:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# NetLogo Crypto Extension
+
+This extension allows you perform cryptography in NetLogo.  Currently, it is not very full-featured, though.
+
+## Building
+
+Use the NETLOGO environment variable to tell the Makefile which NetLogoLite.jar to compile against.  For example:
+
+    NETLOGO=/Applications/NetLogo\\\ 5.0
+    make
+
+If compilation succeeds, `crypto.jar` will be created.
+
+## Terms of Use
+
+[![CC0](http://i.creativecommons.org/p/zero/1.0/88x31.png)](http://creativecommons.org/publicdomain/zero/1.0/)
+
+The NetLogo Crypto extension is in the public domain.  To the extent possible under law, Uri Wilensky has waived all copyright and related or neighboring rights.
---
+++ src/CryptoExtension.scala
@@ -0,0 +1,44 @@
+package org.nlogo.extensions.crypto
+
+import
+  org.nlogo.api.{ Argument, Context, DefaultClassManager, DefaultReporter, PrimitiveManager }
+
+class CryptoExtension extends DefaultClassManager {
+
+  def load(primitiveManager: PrimitiveManager) {
+    primitiveManager.addPrimitive("rsa-encrypt", RSAEncrypt)
+  }
+
+  object RSAEncrypt extends DefaultReporter {
+
+    import
+      java.{ math, security },
+        math.BigInteger,
+        security.{ KeyFactory, spec },
+          spec.RSAPublicKeySpec
+
+    import javax.crypto.Cipher
+    import org.nlogo.api.Syntax
+
+    private val AlgorithmName = "RSA"
+    private val ByteEncoding  = "UTF-8"
+
+    private val factory = KeyFactory.getInstance(AlgorithmName)
+
+    override def getSyntax = {
+      import Syntax.{ StringType => ST }
+      Syntax.reporterSyntax(Array(ST, ST, ST), ST)
+    }
+
+    override def report(args: Array[Argument], context: Context) : AnyRef = {
+      val Seq(input, modulus, exponent) = args.toSeq map (_.getString)
+      val key    = factory.generatePublic(new RSAPublicKeySpec(new BigInteger(modulus), new BigInteger(exponent)))
+      val cipher = Cipher.getInstance(AlgorithmName)
+      cipher.init(Cipher.DECRYPT_MODE, key)
+      new String(cipher.doFinal(input.getBytes(ByteEncoding)), ByteEncoding)
+    }
+
+  }
+
+}
+
